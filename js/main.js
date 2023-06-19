872423cd10100c7d79d4db360d2a1a1ccbedfc43
YuliyaAtrashkevich <trafimava@gmail.com>
2023-06-19 11:01:31 +0600

    Модальные окна, анимация кнопок

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sidenav = document.querySelectorAll('.sidenav');
-    var instances_sidenav = M.Sidenav.init(sidenav);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // Всплывающее меню
+  var sidenav = document.querySelectorAll('.sidenav');
+  var instances_sidenav = M.Sidenav.init(sidenav);
+  // Модальное окно 1
+  var modal1 = document.querySelectorAll('.modal');
+  var instances_modal1 = M.Modal.init(modal1);
+  // Модальное окно 1
+  var modal2 = document.querySelectorAll('.modal');
+  var instances_modal2 = M.Modal.init(modal2);
+});
